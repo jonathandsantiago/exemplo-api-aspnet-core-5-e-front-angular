64044f8c99f47f9069ca12de4616fc6e2e30cbdd
Jonathan Dias Campos Santiago <jonathandcsantiago@outlook.com>
2021-08-20 03:10:17 -0400

    Corrigido o mock do redis no teste unitario após a refatoração dos serviços de cache.

--- favodemel-api/test/FavoDeMel.Tests/Mocks/RedisMock.cs
+++ favodemel-api/test/FavoDeMel.Tests/Mocks/RedisMock.cs
@@ -2,6 +2,7 @@
 using FavoDeMel.Domain.Entities.Usuarios;
 using Microsoft.Extensions.DependencyInjection;
 using Moq;
+using System;
 
 namespace FavoDeMel.Tests.Mocks
 {
@@ -9,15 +10,15 @@ namespace FavoDeMel.Tests.Mocks
     {
         public static IServiceCollection AddRedisMock(this IServiceCollection services)
         {
-            services.AddSingleton(ObterServicoCache<Usuario>());
+            services.AddSingleton(ObterServicoCache<Usuario, Guid>());
 
             return services;
         }
 
-        public static IServiceCache ObterServicoCache<T>()
+        public static IServiceCache ObterServicoCache<T, TId>()
         {
             var mock = new Mock<IServiceCache>();
-            mock.Setup(c => c.ObterAsync<T>(It.IsAny<string>()));
+            mock.Setup(c => c.ObterAsync<T, TId>(It.IsAny<TId>()));
             mock.Setup(c => c.SalvarAsync(It.IsAny<string>(), It.IsAny<T>(), It.IsAny<int>()));
             mock.Setup(c => c.RemoverAsync(It.IsAny<string>()));
 
